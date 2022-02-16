1bc2091d52882aa039ea220c6f1d78e632ef37a8
H-V-N <57254089+H-V-N@users.noreply.github.com>
2022-02-16 09:07:44 -0600

    build out homepage
    -built out homepage
    -added missing dependency for base container
    -fixed bug with image scaling during router transition

--- src/frontend/public-site/src/App.vue
+++ src/frontend/public-site/src/App.vue
@@ -1,15 +1,36 @@
 <template>
   <v-app id="inspire">
     <v-app-bar app flat>
-      <v-app-bar-nav-icon
-        v-if="$vuetify.breakpoint.smAndDown"
-        @click.stop="drawer = !drawer"
-      ></v-app-bar-nav-icon>
+      <div style="width: 310px">
+        <template v-if="isMobile">
+          <dark-button
+            class="mx-2"
+            icon
+            small
+            color="primary"
+            href="https://github.com/H-V-N"
+            target="_blank"
+          >
+            <v-icon>mdi-github</v-icon>
+          </dark-button>
+          <dark-button
+            class="mr-2"
+            icon
+            small
+            color="primary"
+            href="https://www.linkedin.com/in/jack-givens-a531a5163/"
+            target="_blank"
+          >
+            <v-icon>mdi-linkedin</v-icon>
+          </dark-button>
+        </template>
+        <!-- <dark-button v-else color="primary" rounded>
+          <v-icon left>mdi-download</v-icon>
+          Resume
+        </dark-button> -->
+      </div>
 
-      <!--keeps tabs centered... kinda hacky-->
-      <div style="width: 310px" />
-
-      <v-tabs v-if="$vuetify.breakpoint.mdAndUp" centered class="ml-n9">
+      <v-tabs v-if="!isMobile" centered class="ml-n9">
         <v-tab
           v-for="route in routes"
           :key="route.name"
@@ -22,52 +43,92 @@
         </v-tab>
       </v-tabs>
       <v-spacer />
-      <color-picker />
-      <v-btn color="primary" rounded outlined>Hire Me</v-btn>
+      <material-you-picker />
+      <dark-button
+        color="primary"
+        rounded
+        href="mailto:jack.w.givens@gmail.com"
+        target="_blank"
+      >
+        Contact Me
+      </dark-button>
     </v-app-bar>
-    <v-navigation-drawer
-      v-if="$vuetify.breakpoint.smAndDown"
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list nav>
-        <v-list-item
-          v-for="route in routes"
-          :key="route.name"
-          :to="route.path"
-          :href="route.href"
-          :target="route.href && '_blank'"
-        >
-          <v-list-item-title>{{ route.name }}</v-list-item-title>
-          <v-list-item-icon v-if="route.href">
-            <v-icon>mdi-open-in-new</v-icon>
-          </v-list-item-icon>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
 
-    <v-main class="grey lighten-3">
+    <v-main :class="mainClass">
       <router-view />
     </v-main>
+    <v-bottom-navigation v-if="isMobile" app color="primary">
+      <v-btn v-for="route in routes" :key="route.name" :to="route.path">
+        <span>{{ route.name }}</span>
+        <v-icon>{{ route.meta.icon }}</v-icon>
+      </v-btn>
+      <!-- <v-btn
+        href="https://www.linkedin.com/in/jack-givens-a531a5163/"
+        target="_blank"
+      >
+        <span>Resume</span>
+        <v-icon>mdi-download</v-icon>
+      </v-btn> -->
+    </v-bottom-navigation>
+    <v-footer v-else padless fixed color="rgba(0,0,0,0)" app>
+      <div class="d-flex pb-6 pl-3">
+        <dark-button
+          class="mx-2"
+          fab
+          small
+          color="primary"
+          href="https://github.com/H-V-N"
+          target="_blank"
+        >
+          <v-icon>mdi-github</v-icon>
+        </dark-button>
+        <dark-button
+          class="mx-2"
+          fab
+          small
+          color="primary"
+          href="https://www.linkedin.com/in/jack-givens-a531a5163/"
+          target="_blank"
+        >
+          <v-icon>mdi-linkedin</v-icon>
+        </dark-button>
+      </div>
+    </v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import ColorPicker from './components/ColorPicker.vue';
 import { routes } from './router';
 export default Vue.extend({
-  components: { ColorPicker },
   data: () => ({
-    routes: [
-      ...routes,
-      {
-        name: 'GitHub',
-        href: 'https://github.com/H-V-N'
-      }
-    ],
-    drawer: false
-  })
+    routes
+  }),
+  computed: {
+    mainClass() {
+      return {
+        'bg-img': this.$route.name === 'Home',
+        [`img-${this.$vuetify.breakpoint.name}`]: true
+      };
+    },
+    isMobile() {
+      return this.$vuetify.breakpoint.smAndDown;
+    }
+  }
 });
 </script>
+
+<style lang="sass" scoped>
+
+.bg-img
+  background-image: url('./assets/headshot.png')
+  background-size: 600px
+  background-position: bottom
+
+  &.img-md,
+  &.img-sm
+    background-position: bottom right
+
+  &.img-xs
+    background-size: auto 55%
+</style>
--- src/frontend/public-site/src/views/Home.vue
+++ src/frontend/public-site/src/views/Home.vue
@@ -1,31 +1,82 @@
 <template>
-  <v-container>
+  <base-container :class="containerClass">
     <v-row>
-      <v-col cols="12" sm="2">
-        <v-sheet rounded="lg" min-height="268">
-          <!--  -->
-        </v-sheet>
+      <v-col cols="10" sm="5" class="d-flex flex-column text-h1 primary--text">
+        <div>Jack</div>
+        <div>Givens</div>
       </v-col>
-
-      <v-col cols="12" sm="8">
-        <v-sheet min-height="70vh" rounded="lg">
-          <!--  -->
-        </v-sheet>
-      </v-col>
-
-      <v-col cols="12" sm="2">
-        <v-sheet rounded="lg" min-height="268">
-          <!--  -->
-        </v-sheet>
+      <v-spacer />
+      <v-col cols="12" lg="4" class="d-flex flex-column justify-center">
+        <div class="d-flex align-end overflow-x-visible">
+          <div class="text-h6 text-no-wrap">Hi, I'm an experienced</div>
+          <v-slide-y-transition leave-absolute hide-on-leave>
+            <div style="width: 180px" class="flex-grow-1" :key="currentRole">
+              <dark-chip color="accent" class="ml-2">
+                {{ roles[currentRole] }}
+              </dark-chip>
+            </div>
+          </v-slide-y-transition>
+        </div>
+        <span class="text-h6">who loves bringing code to life</span>
       </v-col>
     </v-row>
-  </v-container>
+    <v-row>
+      <v-col cols="3" sm="4">
+        <dark-button color="secondary" to="About">
+          About Me
+          <v-icon right dark>mdi-arrow-right</v-icon>
+        </dark-button>
+      </v-col>
+    </v-row>
+    <v-spacer />
+  </base-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
 export default Vue.extend({
-  name: 'Home'
+  name: 'Home',
+  data: () => ({
+    roles: [
+      'Software Engineer',
+      'UI/UX Designer',
+      'Systems Architect',
+      'Technical Writer',
+      'DevOps Engineer',
+      'Team Lead',
+      'Full-Stack Developer',
+      'Project Manager',
+      'Database Admin'
+    ],
+    currentRole: 0
+  }),
+  computed: {
+    containerClass(): Record<string, boolean> {
+      const active = this.$vuetify.breakpoint.lgAndUp;
+      return {
+        'fill-height': active,
+        'align-start': active
+      };
+    }
+  },
+  mounted() {
+    setInterval(() => this.nextRole(), 2000);
+  },
+  methods: {
+    nextRole() {
+      if (this.currentRole === this.roles.length - 1) {
+        this.currentRole = 0;
+      } else {
+        this.currentRole++;
+      }
+    }
+  }
 });
 </script>
+
+<style lang="scss" scoped>
+.text-h1 {
+  font-weight: 500;
+}
+</style>
