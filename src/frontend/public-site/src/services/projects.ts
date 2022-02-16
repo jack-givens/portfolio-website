34150d99a903daa0fd58751212ed879a622d3415
H-V-N <57254089+H-V-N@users.noreply.github.com>
2022-02-16 17:52:31 -0600

    optimizing images
    -added webp-loader to webpack config to convert images to webp, replaced image references in ts code with require imports

--- src/frontend/public-site/src/services/projects.ts
+++ src/frontend/public-site/src/services/projects.ts
@@ -14,7 +14,7 @@ const projectData: Project[] = [
     subtitle: 'Social Media Advertising',
     description:
       'An advertising platform that allows advertisers to connect with social media influencers',
-    image: './polkie.png',
+    image: require('../assets/polkie.png'),
     tech: [
       'React',
       'React-Native',
@@ -30,7 +30,7 @@ const projectData: Project[] = [
     subtitle: 'Virtual Events Platform',
     description:
       'A multitenant livestreaming platform with realtime chat, group meetings, ticketing integrations, polls, and gamification',
-    image: './vcl2.png',
+    image: require('../assets/vcl2.png'),
     tech: ['Vue', 'ASP.NET Core', 'MSSQL', 'WebSockets', 'WebRTC']
   },
   {
@@ -46,7 +46,7 @@ const projectData: Project[] = [
     subtitle: 'Mobile Rhythm Game',
     description:
       'A competitive, multiplayer rhythm game akin to stepmania or tap-tap revenge, for IOS and Android',
-    image: './Capture.PNG',
+    image: require('../assets/rhythm.png'),
     tech: ['Unity3D', 'Firebase']
   },
   {
@@ -54,7 +54,7 @@ const projectData: Project[] = [
     subtitle: 'Machine-Learning Playlist Creator',
     description:
       'A small proof-of-concept site that generates a recommended playlist based off of a seed song',
-    image: './vm2.png',
+    image: require('../assets/vm2.png'),
     tech: ['Vue', 'ASP.NET Core', 'Machine Learning', 'KNN']
   },
   {
@@ -62,7 +62,7 @@ const projectData: Project[] = [
     subtitle: 'Patient Portal',
     description:
       'A cross-platform portal for patients to submit forms, contact their doctor, upload documents, and view medication schedules',
-    image: './connect.png',
+    image: require('../assets/connect.png'),
     tech: ['Vue', 'NativeScript-Vue', 'Firebase Cloud Messaging', 'ExpressJS']
   }
 ];
--- src/frontend/public-site/vue.config.js
+++ src/frontend/public-site/vue.config.js
@@ -5,5 +5,16 @@ module.exports = {
       args[0].title = 'Jack Givens';
       return args;
     });
+    const imgRule = config.module.rule('images');
+    imgRule.uses.clear();
+    imgRule
+      .test(/\.(png|jpe?g)(\?.*)?$/)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({ name: 'img/[name].[hash:8].webp' })
+      .end()
+      .use('webp-loader')
+      .loader('webp-loader')
+      .end();
   }
 };
